a7755259a240f2e250c62a85f1fd4b1994ecaeb2
Lamminhtuan <tuanlam300@gmail.com>
2022-10-31 21:00:35 +0700

    dsa

--- line_detection.py
+++ line_detection.py
@@ -1,8 +1,7 @@
-import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import random
-a = cv2.imread('images/sea.jpg')
+
 x = np.arange(7)
 y = 5 * x + 3
 x_noise = np.random.randint(10, size=3)
@@ -10,18 +9,51 @@ y_noise = np.random.randint(40, size=3)
 x = np.append(x, x_noise)
 y = np.append(y, y_noise)
 cor = list(zip(x, y))
+n = len(cor)
+n_oline = int(0.7 * n)
 cor_ = cor.copy()
+def plot_line(a, b, c, x_range, y_range):
+    if b != 0:
+        x_ = np.arange(x_range)
+        y_ = (a * x_ + c) / -b
+    else:
+        x_ = np.ones(y_range) * (-c/a)
+        y_ = np.arange(y_range)
+
+    plt.scatter(x, y)
+    plt.plot(x_, y_)
+    plt.show(block=False)
+    plt.pause(1)
+    plt.close()
+
+
+def gcd(x, y):
+    while(y):
+        x, y = y, x % y
+    return x
+
+
 def line_equation(cor1, cor2):
     x1, y1 = cor1
     x2, y2 = cor2
     a = y2 - y1
+
     b = x1 - x2
     c = -(a*x1 + b*y1)
+    gcd_1 = gcd(a, b)
+    gcd_all = gcd(gcd_1, c)
+    a = int(a / gcd_all)
+    b = int(b / gcd_all)
+    c = int(c / gcd_all)
     return a, b, c
+
+
 def checkalongline(cor, a, b, c):
     x, y = cor
     k = a * x + b * y + c
     return k == 0
+
+
 while True:
     count = 2
     cor = cor_.copy()
@@ -29,20 +61,14 @@ while True:
     cor1 = cor.pop()
     cor2 = cor.pop()
     a, b, c = line_equation(cor1, cor2)
+    plot_line(a, b, c, 11, 41)
     for i in cor:
         if checkalongline(i, a, b, c) == True:
             count += 1
-    if count == 7:
-        fig = plt.figure()
-        x1, y1= cor1
-        x2, y2 = cor2
-        plt.scatter(x1, y1)
-        plt.scatter(x2, y2)
-        plt.plot((x1, x2), (y1, y2))
-        plt.show()
-        if (b < 0):
-            print(a, 'x ', b, ' = ', c)
-        else:
-            print(a, 'x + ', b, ' = ', c)
 
+    if count == n_oline:
+        if (b < 0):
+            print('{0}x{1}y={2}'.format(a, b, c))
+        else:
+            print('{0}x+{1}y={2}'.format(a, b, c))
         break
--- test1.py
+++ test1.py
@@ -1,11 +1,8 @@
 import numpy as np
-import cv2 
-a  = np.array([[7, 8, 27, 20],
-                [3, 4, 3, 25],
-                [5, 6, 22, 21]], dtype=np.int16)
-def calculate_gradient(a):
-    b_n = (np.pad(a[1:, :], ((0, 1), (0, 0)), 'constant') - a) ** 2
-    c_n = (np.pad(a[:, 1:], ((0, 0), (0,1)), 'constant') - a) ** 2
-    result = np.sqrt(b_n + c_n)
-    return result
-print(calculate_gradient(a))
+import matplotlib.pyplot as plt
+x = np.arange(7)
+y = -3 * x + 3
+fig = plt.figure()
+plt.plot(x, y)
+plt.show(block=False)
+plt.close()
