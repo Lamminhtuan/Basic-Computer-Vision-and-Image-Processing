7fe07dedea2ad864c8c29cdfd9645fe97e90cea4
Lamminhtuan <tuanlam300@gmail.com>
2022-11-01 13:27:45 +0700

    Update 20520843_CS231.N11_BT5.py

--- 20520843_CS231.N11_BT5.py
+++ 20520843_CS231.N11_BT5.py
@@ -1,24 +1,44 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+
 n = int(input('Enter n: '))
+a_ = float(input('Enter a: '))
+b_ = float(input('Enter b: '))
+while a_ == 0 and b_ == 0:
+    a_ = float(input('Enter a: '))
+    b_ = float(input('Enter b: '))
+
+c_ = float(input('Enter c: '))
+if (b_ < 0):
+    print('Line\'s Equation entered: {0}x{1}y={2}'.format(a_, b_, c_))
+else:
+    print('Line\'s Equation entered: {0}x+{1}y={2}'.format(a_, b_, c_))
 n_line = int(0.7 * n)
 n_non = n - n_line
-x = np.linspace(0, n * 10, n_line, dtype=int)
-y = 5 * x + 3
-x_noise = np.random.randint(n * 10, size=n_non)
-y_noise = np.random.randint(n * 40, size=n_non)
+if b_ != 0:
+    x = np.linspace(0, n, n_line, dtype=int)
+    y = (a_ * x + c_) / -b_
+else:
+    x = np.ones(n) * (-c_/a_)
+    y = np.arange(n)
+
+x_noise = np.random.randint(n, size=n_non)
+y_noise = np.random.randint(n, size=n_non)
+
+
 x_zip = np.append(x, x_noise)
 y_zip = np.append(y, y_noise)
 cor = list(zip(x_zip, y_zip))
 cor_ = cor.copy()
-def plot_line(a, b, c, x_range, y_range):
+
+def plot_line(a, b, c):
     if b != 0:
-        x_ = np.arange(x_range)
+        x_ = np.arange(n * 2)
         y_ = (a * x_ + c) / -b
     else:
-        x_ = np.ones(y_range) * (-c/a)
-        y_ = np.arange(y_range)
+        x_ = np.ones(n * 2) * (-c/a)
+        y_ = np.arange(n * 2)
 
     plt.scatter(x, y, color = 'g')
     plt.scatter(x_noise, y_noise, color = 'r')
@@ -43,9 +63,9 @@ def line_equation(cor1, cor2):
     c = -(a*x1 + b*y1)
     gcd_1 = gcd(a, b)
     gcd_all = gcd(gcd_1, c)
-    a = int(a / gcd_all)
-    b = int(b / gcd_all)
-    c = int(c / gcd_all)
+    a = float(a / gcd_all)
+    b = float(b / gcd_all)
+    c = float(c / gcd_all)
     return a, b, c
 
 
@@ -62,13 +82,13 @@ while True:
     cor1 = cor.pop()
     cor2 = cor.pop()
     a, b, c = line_equation(cor1, cor2)
-    plot_line(a, b, c, n * 10, n * 40)
+    plot_line(a, b, c)
     for i in cor:
         if checkalongline(i, a, b, c) == True:
             count += 1
     if count == n_line:
         if (b < 0):
-            print('{0}x{1}y={2}'.format(a, b, c))
+            print('Line\'s Equation found: {0}x{1}y={2}'.format(a, b, c))
         else:
-            print('{0}x+{1}y={2}'.format(a, b, c))
+            print('Line\'s Equation found: {0}x+{1}y={2}'.format(a, b, c))
         break
