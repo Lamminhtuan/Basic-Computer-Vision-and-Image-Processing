929bc247b63243718e9cc30f86b40aa6fc668743
Lamminhtuan <tuanlam300@gmail.com>
2022-12-21 16:43:36 +0700

    add sth

---
+++ contour_video.py
@@ -0,0 +1,54 @@
+import cv2
+import numpy as np
+cap = cv2.VideoCapture('./images/video.mp4')
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        print('Het video')
+        break
+    
+    h = frame.shape[0]
+    w = frame.shape[1]
+
+    img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    kernel = np.ones((1,1), np.uint8)
+    kernel_dil = np.ones((3,3), np.uint8)
+    img = cv2.GaussianBlur(img, (3,3), 0)
+    canny = cv2.Canny(img, 10, 100)
+    # ret, thresh = cv2.threshold(img, np.mean(img), 255, cv2.THRESH_BINARY_INV)
+    # edges = cv2.erode(canny, kernel, iterations = 1)
+    edges = cv2.dilate(canny, kernel_dil, iterations = 1)
+    cnt = sorted(cv2.findContours(canny, mode=cv2.RETR_CCOMP, method=cv2.CHAIN_APPROX_SIMPLE)[-2], key=cv2.contourArea)[-1]
+    # contours, _ = cv2.findContours(edges, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_SIMPLE)
+    mask = np.zeros((h, w), np.uint8)
+    # masked = cv2.drawContours(mask, [cnt], -1, 255, -1)
+    # dst = cv2.bitwise_and(frame, frame, mask=mask)
+
+    cv2.imshow('frame', cnt)
+    if cv2.waitKey(25) == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
+# ori = cv2.imread('./images/cf.jpg')
+# h = ori.shape[0]
+# w = ori.shape[1]
+# #Convert to grayscale
+# img = cv2.cvtColor(ori, cv2.COLOR_BGR2GRAY)
+# # img = cv2.add(img, 10)
+# #Binarizing
+# kernel = np.ones((1,1), np.uint8)
+# kernel_dil = np.ones((3,3), np.uint8)
+# img = cv2.GaussianBlur(img, (3,3), 0)
+# canny = cv2.Canny(img, 10, 100)
+# # ret, thresh = cv2.threshold(img, np.mean(img), 255, cv2.THRESH_BINARY_INV)
+# # edges = cv2.erode(canny, kernel, iterations = 1)
+# edges = cv2.dilate(canny, kernel_dil, iterations = 1)
+# cnt = sorted(cv2.findContours(edges, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_SIMPLE)[-2], key=cv2.contourArea)[-1]
+# # contours, _ = cv2.findContours(edges, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_SIMPLE)
+# mask = np.zeros((h, w), np.uint8)
+# masked = cv2.drawContours(mask, [cnt], -1, 255, -1)
+# dst = cv2.bitwise_and(ori, ori, mask=mask)
+# cv2.imshow('Mask', canny)
+# cv2.imshow('Result', dst)
+# cv2.waitKey(0)
--- morphology.py
+++ morphology.py
@@ -1,11 +1,43 @@
 import cv2
 import numpy as np
-s = cv2.imread('images/face.jpg')
-d = cv2.imread('images/cat.jpg')
-d = cv2.resize(d, (s.shape[1], s.shape[0]))
-for f in range(0, 3*24):
-    t = f / (3 * 24)
-    tt = s.copy()
-    tt = ((1-t) * s + t * d).astype(np.uint8)
-    cv2.imshow('result', tt)
-    cv2.waitKey(10)
+ori = cv2.imread('./images/tebaomau.png')
+img = cv2.cvtColor(ori, cv2.COLOR_BGR2GRAY)
+#Nhị phân hóa ảnh
+ret, img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+img = cv2.bitwise_not(img)
+kernel = np.ones((30, 30), np.uint8)
+kernel_ero_1 = np.ones((10, 10), np.uint8)
+kernel_ero_2 = np.ones((4, 4), np.uint8)
+kernel_ero_3 = np.ones((2, 2), np.uint8)
+opening = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+erosion = cv2.erode(opening,kernel_ero_1,iterations = 1)
+erosion = cv2.erode(erosion,kernel_ero_2,iterations = 1)
+erosion = cv2.erode(erosion,kernel_ero_3,iterations = 1)
+contour_img = ori.copy()
+contours = cv2.findContours(erosion, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+contours = contours[0] if len(contours) == 2 else contours[1]
+index = 1
+isolated_count = 0
+cluster_count = 0
+for cntr in contours:
+    area = cv2.contourArea(cntr)
+    convex_hull = cv2.convexHull(cntr)
+    convex_hull_area = cv2.contourArea(convex_hull)
+    ratio = area / convex_hull_area
+    if ratio < 0.91:
+        #Liên thông màu đỏ
+        cv2.drawContours(contour_img, [cntr], 0, (0,0,255), 2)
+        cluster_count = cluster_count + 1
+    else:
+        #Độc lập màu xanh
+        cv2.drawContours(contour_img, [cntr], 0, (0,255,0), 2)
+        isolated_count = isolated_count + 1
+    index = index + 1
+    
+print('number_clusters:',cluster_count)
+print('number_isolated:',isolated_count)
+print('Number of blood cells', cluster_count + isolated_count)
+cv2.imshow("erosion", erosion)
+cv2.imshow("result", contour_img)
+cv2.waitKey(0)
+
