b6ea5939326e4d483c8ebb96648529d91b7f399f
Lâm Minh Tuấn <tuanlam300@gmail.com>
2023-01-25 17:28:00 +0700

    ADD

--- contour_final.py
+++ contour_final.py
@@ -4,7 +4,6 @@ import time
 from statistics import mean
 #Đường dẫn chứa file test
 cap = cv2.VideoCapture('./images/test.mp4')
-bg = cv2.imread('./images/background.jpg')
 def auto_canny(image, sigma = 0.33):
     v = np.median(image)
     lower = int(max(0, (1.0 - sigma) * v))
@@ -15,7 +14,6 @@ def preprocessing(image):
     kernel_dil = np.ones((50,50), np.uint8)
     edges = cv2.dilate(image, kernel_dil, iterations = 1)
     return edges
-flag = False
 size = (1280, 720)
 while cap.isOpened():
     ret, frame = cap.read()
@@ -24,8 +22,6 @@ while cap.isOpened():
     frame = cv2.resize(frame, size)
     h = frame.shape[0]
     w = frame.shape[1]
-    if flag == False:
-        bg = cv2.resize(bg, (w, h))
     img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     img = cv2.GaussianBlur(img, (3,3), 1)
     thresh = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 13, 5)
@@ -43,9 +39,6 @@ while cap.isOpened():
         masked = cv2.erode(masked, kernel, iterations = 1)
         masked = cv2.erode(masked, kernel_1, iterations = 3)
         dst_fg = cv2.bitwise_and(frame, frame, mask=masked)
-        mask_inversed = cv2.bitwise_not(masked)
-        dst_bg = cv2.bitwise_and(bg, bg, mask=mask_inversed)
-        dst = cv2.bitwise_or(dst_fg, dst_bg)
         cv2.imshow('frame', dst_fg)
     else:
         cv2.imshow('frame', frame)
